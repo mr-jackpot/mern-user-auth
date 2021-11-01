cee27db2d895bf14f1bfdde14c321ff6d91db883
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-11-01 22:49:08 +0000

    added Debug package to server.js

--- src/server/server.js
+++ src/server/server.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+const debug = require('debug');
+const log = debug('http:server');
+const http = require('http');
+const name = '[server.js]';
+
+log('Starting: ', name);
+
 app.get("/", (req, res) => res.send("Boom, you've hit the express server"));
 
-app.listen(port, () => console.log(
-  `Example app listening on port ${port}!`
-));
+// app.listen(port, () => console.log(
+//   `[server.js] Server listening on port ${port}.`
+// ));
+
+app.listen(port, () => {
+    log(`[server.js] Server listening on port ${port}.`);
+})
