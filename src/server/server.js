8d964960a9f6710f7abb4fe4bf64214e6fc02b50
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-11-01 22:24:18 +0000

    set nodemon verbosity to false

--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
-app.get("/", (req, res) => res.send("Good monring sunshine!"));
+app.get("/", (req, res) => res.send("Boom, you've hit the express server"));
 
 app.listen(port, () => console.log(
   `Example app listening on port ${port}!`
