af10c541b9fa27c5fda89b629b68988dfd0f9adf
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-11-02 18:19:12 +0000

    added chalk and updated server logging

--- src/server/server.js
+++ src/server/server.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+const chalk = require('chalk')
 const debug = require('debug');
 const log = debug('http:server');
 const http = require('http');
 const name = '[server.js]';
-
-log('Starting: ', name);
+const serverLog = chalk.redBright.bold
 
 app.get("/", (req, res) => res.send("Boom, you've hit the express server"));
 
 app.listen(port, () => {
-    log(`${name} listening on port ${port}.`);
+    log(serverLog(`Starting: ${name} listening on port ${port}.`));
 })
