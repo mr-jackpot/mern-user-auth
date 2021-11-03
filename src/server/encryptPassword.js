ed6aa6c318b87373d1f3293c37f5208bd0c92b33
Adam Heeps <adamheeps@adams-mbp.home>
2021-11-03 20:44:42 +0000

    still bugged

--- src/server/encryptPassword.js
+++ src/server/encryptPassword.js
@@ -1,22 +1,25 @@
 const bcrypt = require('bcryptjs')
 const saltRounds = 10;
+var hashReturn;
+
+//TODO: THIS FUNCTION DOES NOT RETURN THE VALUE BACK TO THE SERVER.JS CALL
 
 const encrypt = (password) => {
 
-    bcrypt.genSalt(saltRounds, (saltError, salt) => {
+    bcrypt.genSalt(saltRounds, function (saltError, salt) {
         if (saltError) {
-            throw saltError;
+          throw saltError
         } else {
-            bcrypt.hash(password, salt, (hashError, hash) => {
-                if (hashError) {
-                    throw hashError;
-                } else {
-                    return hash;
-                }
-            })
+          bcrypt.hash(password, salt, function(hashError, hash) {
+            if (hashError) {
+                throw hashError
+            } else {
+                //return hash;
+                console.log(hash)
+            }
+          })
         }
-    })  
- 
+    })
 }
 
 module.exports = {encrypt};
