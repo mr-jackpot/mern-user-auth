ee29fde82db4f88b9df355f0e9fbed01b503620b
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-11-03 16:58:31 +0000

    added preventdefault - refreshing issue resolve

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,10 +5,11 @@ const Login = () => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
         console.log(`Username: ${username}`)
         console.log(`Password: ${password}`)
-
+        // to api
     }
     
     return (
@@ -16,11 +17,13 @@ const Login = () => {
             <form onSubmit={handleSubmit}>
                 <label>Username:</label><br/>
                 <input
+                    value={username}
                     onChange={(u) => setUsername(u.target.value)}
                     required
                 /><br/>
                 <label>Password:</label><br/>
                 <input
+                    value={password}
                     onChange={(p) => setPassword(p.target.value)}
                     type="password"
                     required
