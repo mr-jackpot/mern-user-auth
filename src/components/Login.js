023a6a03fbc2201bdaa3218cecd8398ca4ac4133
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-11-03 08:47:25 +0000

    Login component created, bug where the page is refreshing after submit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import {React, useState } from 'react'
 
 const Login = () => {
+    
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = () => {
+        console.log(`Username: ${username}`)
+        console.log(`Password: ${password}`)
+
+    }
+    
     return (
         <div>
-            <h3>Username</h3>
-            <input></input>
-            <h3>Password</h3>
+            <form onSubmit={handleSubmit}>
+                <label>Username:</label><br/>
+                <input
+                    onChange={(u) => setUsername(u.target.value)}
+                    required
+                /><br/>
+                <label>Password:</label><br/>
+                <input
+                    onChange={(p) => setPassword(p.target.value)}
+                    type="password"
+                    required
+                /><br/>
+                <input type="submit" value="Submit"></input>
+            </form>
         </div>
     )
 }
