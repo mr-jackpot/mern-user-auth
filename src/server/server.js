6080bd5f46e4c405c7b46198e86141b44acd8fff
Adam Heeps <adamheeps@adams-mbp.home>
2021-11-08 17:02:22 +0000

    re-baseline. Please review and PR

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 4000;
-const authenticate = require("./auth");
 const config = require("./config.js");
 
-const session = require('express-session');
-const passport = require('passport');
 const userSchema = require("../models/users");
 const mongoose = require("mongoose");
 const chalk = require("chalk");
@@ -14,8 +11,8 @@ const log = debug("http:server");
 const http = require("http");
 const name = "[server.js]";
 const serverLog = chalk.redBright.bold;
-const authSuccess = chalk.greenBright.bold;
-const authFailure = chalk.yellowBright.bold;
+const greenLog = chalk.greenBright.bold;
+const yellowLog = chalk.yellowBright.bold;
 
 const db = `mongodb+srv://${config.mongoUser}:${config.mongoPassword}@${config.mongoCluster}/${config.mongoDatabase}?retryWrites=true&w=majority`;
 
@@ -29,26 +26,6 @@ mongoose
     )
   )
   .catch((err) => log(serverLog(err)));
-
-passport.use(authenticate.strategy);
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser((user, done) => {
-  done(null, user.username);
-});
-
-passport.deserializeUser(async (email, done) => {
-  try {
-    let user = await userSchema.findOne(user);
-    if (!user) {
-      return done(new Error('user not found'));
-    }
-    done(null, user);
-  } catch (e) {
-    done(e);
-  }
-});
 
 app.use((req, res, next) => {
   // Authorizing API call to come from React front end on port 3000
@@ -80,13 +57,9 @@ app.get("/", (req, res) => {
 });
 
 app.post("/auth", (req, res) => {
-  app.post('/login',
-  passport.authenticate('local', { successRedirect: '/' }));
+
 });
 
 app.listen(port, () => {
   log(serverLog(`${name} running on port ${port}.`));
 });
-
-// app.use(passport.initialize());
-// app.use(passport.session());
