fbb2d6bf80894014810d1a99926e53f90940e3de
Adam Heeps <adamheeps@adams-mbp.home>
2021-11-08 21:07:21 +0000

    added login route frontend

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login'
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import LoginRedirect from './pages/LoginRedirect/LoginRedirect';
 
 function App() {
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Enter your username and password.
-        </p>
-        <Login />
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Enter your username and password.
+              </p>
+              <Login />
+            </header>
+          </div>
+        </Route>
+        <Route path="/session">
+          <LoginRedirect />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/LoginRedirect/LoginRedirect.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginRedirect = () => {
+
+    return (
+        <div>
+            <h1>Success! You are now logged in.</h1>
+        </div>
+    )
+}
+
+export default LoginRedirect;
