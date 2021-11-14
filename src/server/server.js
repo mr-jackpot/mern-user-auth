1de7b46aee1f4b9d4b5e16df79d0209dee38dd13
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-11-14 22:36:01 +0000

    We have a working cookie and session set up - and can log the number of times a cookie has access a route

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+
 const config = require("./config.js");
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
 
 const userSchema = require("../models/users");
 const mongoose = require("mongoose");
@@ -16,10 +19,8 @@ const greenLog = chalk.greenBright.bold;
 const yellowLog = chalk.yellowBright.bold;
 
 // Passport
-const session = require("express-session");
 const passport = require('passport')
 const LocalStrategy =  require('passport-local').Strategy;
-const { request } = require("express");
 
 //Database config
 const db = `mongodb+srv://${config.mongoUser}:${config.mongoPassword}@${config.mongoCluster}/${config.mongoDatabase}?retryWrites=true&w=majority`;
@@ -34,6 +35,11 @@ mongoose
   )
   .catch((err) => log(serverLog(err)));
 
+// const sessionStore = new MongoStore({
+//   mongooseConnection: connection, 
+//   collection: 'sessions'
+// });
+
 // **** middleware ****//
 app.use((req, res, next) => {
   // Authorizing API call to come from React front end on port 3000
@@ -47,15 +53,18 @@ app.use((req, res, next) => {
     "X-Requested-With,content-type"
   );
   res.setHeader("Access-Control-Allow-Credentials", true);
-  next();});
+  next();
+});
+
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true,}));
 app.use(express.json());
-app.use(session({ //setup datastore in session
+app.use(session({ //setup session middleware
   secret: "secret",
+  resave: false,
   saveUninitialized: true,
-  cookie: {maxAge: 360000},
-  resave: false
+  // store: sessionStore, we'll want to set up sessionStore with connect-mongo for production
+  cookie: {maxAge: 180000},
 })); // Set up express-sessions
 app.use(passport.initialize()); // initialize passport + sessions
 app.use(passport.session()); // initialize passport + sessions
@@ -69,7 +78,6 @@ passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
-app.listen(port, () => {log(serverLog(`${name} running on port ${port}.`));});
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
@@ -87,49 +95,66 @@ passport.use(new LocalStrategy(
     );
   }
 ));
+// this was a .post...
 app.post('/auth',
-  passport.authenticate('local'),
+  passport.authenticate('local'), // we're passing passport middleware here.
     function(req, res) {
       // If this function gets called, authentication was successful.
-      log(greenLog(`Authentication Successful`))
-      log(greenLog(req.user)) // user object from db!
-      log(greenLog(req.isAuthenticated())) // user object from db
+      log(greenLog(`Authentication Successful: ${req.isAuthenticated()}`))
+      // log(greenLog(req.user)) // user object from db!
+      // log(greenLog(req.session.cookie)) // user object from db!
+      // log(greenLog(req.isAuthenticated())) // user object from db
       // res.json() // we can pass 
-      res.redirect('/session'); // lets get the redirect to change
+      res.cookie().redirect('/protected');
+      // res.redirect('/protected'); // lets get the redirect to change
     })
-  log(greenLog(`[Server.js] Line 72 firing`))
-  ;
+  log(greenLog(`[Server.js] Line 72 firing`));
 
-app.get("/session",  (req, res) => {
-  //lets hide this session unless there's a cookie
-  log(yellowLog('Are we authenticated here?')) // user object from db!
-  if (req.user) {
-    log(yellowLog("Yes"))
+app.get("/", (req, res, next) => {
+  //returns a sessions cookie but doesn't log network info...
+  if (req.session.viewCount) {
+    req.session.viewCount++;
   } else {
-    log(yellowLog("No"))
+    req.session.viewCount = 1;
   }
-  log(yellowLog(req.user)) // user object from db!
-  log(yellowLog(req.user)) // user object from db!
-  log(yellowLog(req.isAuthenticated())) // user object from db
-
-  // [troubleshooting] - we need to find where the cookie is stored, or is it passed into this.
-  // if (!req.cookies.token) {
-  if (req.cookies['connect.sid']) {
-    log(serverLog(`Cookie found`));
-    return res.status(401).send("401: Unathorised access attempt");} //.send(), this should parse for a cookie
-  
-  log(greenLog('Welcome to the session page - user routed'))
-  res.json()
+  console.log(`Here's our cookie! ${req.session}, it's been used ${req.session.viewCount} times`)
+  res.status(200).send(`Here's a cookie - you have used it ${req.session.viewCount} times`)
+  next();
 })
 
+// app.get("/session",  (req, res) => {
+//   //lets hide this session unless there's a cookie
+//   log(yellowLog('Are we authenticated here?')) // user object from db!
+//   log(yellowLog(req.isAuthenticated())) // user object from db
+
+//   // [troubleshooting] - we need to find where the cookie is stored, or is it passed into this.
+//   // There is a cookie on server side, we need to set this cookie at the client end... 
+//   // if (!req.cookies.token) {
+//   // if (req.cookies['connect.sid']) {
+//   //   log(serverLog(`Cookie found`));
+//   //   res.status(401).send("401: Unathorised access attempt");} //.send(), this should parse for a cookie
+  
+//   log(greenLog('Welcome to the session page - user routed'))
+//   res.json()
+// })
+
 // **** API Requests **** //
-app.get("/", (req, res) => {
-  log(serverLog(`${name} returned a response @ '/' status ${res.statusCode}`));
-  log(serverLog(`We're hitting '/' - the gateway to the server?`));
-  log(serverLog(req.cookies['connect.sid'])); // logs session cookie id
-  log(serverLog(req.cookies)); // logs session cookie id
-  res.send("Boom, you've hit the express server");
-});
+// app.get("/", (req, res) => {
+//   log(serverLog(`${name} returned a response @ '/' status ${res.statusCode}`));
+//   log(serverLog(`We're hitting '/' - the gateway to the server?`));
+//   log(serverLog(req.cookies['connect.sid'])); // logs session cookie id
+//   log(serverLog(req.cookies)); // logs session cookie id
+//   res.send("Boom, you've hit the express server");
+// });
+
+// app.get("/protected", (req, res, next) => {
+//   //returns a sessions cookie but doesn't log network info...
+//   res.send('Here\'s a cookie')
+
+// })
+
+app.listen(port, () => {log(serverLog(`${name} running on port ${port}.`));});
+
 
 // app.get("/*", function(req, res, next) {
 
