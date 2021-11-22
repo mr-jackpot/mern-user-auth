290db804a271a0fa9171a6727460933fad0c1d33
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-11-22 22:26:12 +0000

    changed npm test and attempted to test auth route

--- cypress/integration/homepageTests/backend.spec.js
+++ cypress/integration/homepageTests/backend.spec.js
@@ -3,6 +3,10 @@ const env = require('../../../src/server/props')
 
 describe("tests authenication", () => {
 
-    // taking the dog out... brb
-
+    it("tests authentication fails"), () => {
+      cy.request('POST', `${env.SERVER_URL}${env.SERVER_PORT}/auth`, {'username': 'Admin1', 'password': 'Admin1'})
+      .then((response) => {
+        expect(response.body).to.have.property('code', 200);
+      })
+    }
 })
