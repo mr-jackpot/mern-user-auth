3e2b65695265d2a7accfaba58d12ab0a3955d212
Adam Heeps <adamheeps@adams-mbp.home>
2021-11-26 17:15:43 +0000

    started tryingto get it to work on app engine

--- src/server/props.js
+++ src/server/props.js
@@ -6,11 +6,11 @@ module.exports = {
   SESSION_DB: "session_store",
 
   SERVER_PORT: 4000,
-  SERVER_URL: "http://localhost:",
+  SERVER_URL: "https://fircohard-open-source.nw.r.appspot.com",
   SERVER_NAME: "[server.js]",
 
   REACT_PORT: 3000,
-  REACT_URL: "http://localhost:",
+  REACT_URL: "https://fircohard-open-source.nw.r.appspot.com",
 
   SESSION_SECRET: require("crypto").randomBytes(16).toString('hex'),
 };
